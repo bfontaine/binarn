c9a19517787aeb181839a73ba27f0dabce2fa77d
Baptiste Fontaine <baptiste@bixoto.com>
2022-03-16 12:21:24 +0100

    add some doc

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # z
 
+**z** is a small tool to find an arbitrary key in a deep dictionary. It returns the full path to
+that key (if any) as well as its value.
+
+It is intended as a debugging/exploration tool only.
+
 ## Install
 
     pip install z
--- z/__init__.py
+++ z/__init__.py
@@ -1,4 +1,4 @@
-from typing import Any, Iterable
+from typing import Any, Iterable, Tuple, Optional
 
 __version__ = "0.0.1"
 __all__ = [
@@ -6,15 +6,18 @@ __all__ = [
 ]
 
 _none = object()
+Result = Tuple[tuple, Any]
 
 
-def _find(source, key, value, path=()):
+def _find(source, key, value, path=()) -> Iterable[Result]:
     if value is not _none and source == value:
         yield path, value
 
     if isinstance(source, dict):
         if key is not _none and key in source:
-            yield path + (key,), source[key]
+            _value = source[key]
+            if value is _none or value == _value:
+                yield path + (key,), source[key]
 
         for k, v in source.items():
             yield from _find(v, key, value, path=path + (k,))
@@ -28,9 +31,25 @@ def _find(source, key, value, path=()):
 
 
 def find(source: Any, *, key: Any = _none, value: Any = _none):
+    """
+    Find all usages of a key (or value).
+
+    :param source: dictionary to search in
+    :param key: key search to for
+    :param value: optional value to search for
+    :return:
+    """
     return _find(source, key=key, value=value)
 
 
-def find_one(source: Any, *, key: Any = _none, value: Any = _none):
+def find_one(source: Any, *, key: Any = _none, value: Any = _none) -> Optional[Result]:
+    """
+    Find a single key (or value) usage.
+
+    :param source: dictionary to search in
+    :param key: key search to for
+    :param value: optional value to search for
+    :return: a tuple of (path_to_the_key, value), or None
+    """
     for res in find(source, key=key, value=value):
         return res
